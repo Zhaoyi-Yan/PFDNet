5dcc265b3c728194cc87e14a98867aa6544df5bd
Zhaoyi-Yan <yanzhaoyi@outlook.com>
2021-01-10 12:18:10 +0800

    Update SHA_test.sh

--- SHA_test.sh
+++ SHA_test.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python test.py --dataset_name='SHA' --mode='crop' --nThreads=1 --gpu_ids='0' --batch_size=1 --net_name='csrpersp_crop' --test_model_name='out_models/MAE_53.78_MSE_89.23_Epoch_331.pth'
+python test.py --dataset_name='SHA' --mode='crop' --nThreads=1 --gpu_ids='0' --batch_size=1 --net_name='csrpersp_crop' --test_model_name='model_path'
